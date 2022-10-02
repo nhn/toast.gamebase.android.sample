193cc7c26a4605e738a80c0ad35ce2f618d8dd10
박은우/게임플랫폼클라팀/NE <eunwoo.park@nhn.com>
2022-10-02 23:20:22 +0900

    Add Drawer to MainActivity

--- app/src/main/java/com/nhn/android/gamebase/sample/MainActivity.kt
+++ app/src/main/java/com/nhn/android/gamebase/sample/MainActivity.kt
@@ -3,41 +3,119 @@ package com.nhn.android.gamebase.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.nhn.android.gamebase.sample.ui.HomeScreen
 import com.nhn.android.gamebase.sample.ui.theme.GamebaseSampleProjectTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GamebaseSampleProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background,
+    ) {
+        Scaffold (
+            topBar = {
+                com.nhn.android.gamebase.sample.AppBar {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            },
+            scaffoldState = scaffoldState,
+            drawerContent = {
+                MainDrawer(onDestinationClicked = { /* TODO */ })
+            },
+        ){
+            HomeScreen()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    GamebaseSampleProjectTheme {
-        Greeting("Android")
+fun AppBar(openDrawer: () -> Unit) {
+    TopAppBar(
+        title = { Text("Home") },
+        backgroundColor = MaterialTheme.colors.primary,
+        navigationIcon = {
+            IconButton(onClick = { openDrawer() }) {
+                Icon(Icons.Filled.Menu, "list")
+            }
+        },
+    )
+}
+
+private val screens = listOf(
+    "Home",
+    "Shopping",
+    "Profile",
+    "WebView",
+    "Settings",
+    "Developer"
+)
+
+@Composable
+fun MainDrawer(
+    onDestinationClicked: (route: String) -> Unit
+) {
+    Column(
+        Modifier
+            .wrapContentWidth()
+            .padding(start = 24.dp, top = 48.dp)
+    ) {
+        screens.forEach { screen ->
+            Spacer(Modifier.height(24.dp))
+            Text(
+                text = screen,
+                style = MaterialTheme.typography.body1
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun DrawablePreview() {
+    MainDrawer {}
 }
