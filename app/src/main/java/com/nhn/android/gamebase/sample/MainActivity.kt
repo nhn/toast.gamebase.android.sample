9a598107871fc59becac7d9dd9efac08ff91fe10
박은우/게임플랫폼클라팀/NE <eunwoo.park@nhn.com>
2022-10-04 10:34:50 +0900

    Add navigation to drawer

--- app/src/main/java/com/nhn/android/gamebase/sample/MainActivity.kt
+++ app/src/main/java/com/nhn/android/gamebase/sample/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.nhn.android.gamebase.sample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +16,7 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Snackbar
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -21,17 +24,27 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.nhn.android.gamebase.sample.ui.HomeScreen
+import com.nhn.android.gamebase.sample.ui.ProfileScreen
+import com.nhn.android.gamebase.sample.ui.ShoppingScreen
 import com.nhn.android.gamebase.sample.ui.theme.GamebaseSampleProjectTheme
 import kotlinx.coroutines.launch
 
@@ -46,10 +59,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
+    val navController = rememberNavController()
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -65,18 +80,60 @@ fun MainScreen() {
             },
             scaffoldState = scaffoldState,
             drawerContent = {
-                MainDrawer(onDestinationClicked = { /* TODO */ })
+                MainDrawer { route ->
+                    scope.launch {
+                        scaffoldState.drawerState.close()
+                    }
+                    try {
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    } catch (exception: IllegalArgumentException) {
+                        navController.navigate(SampleAppScreen.Home.route)
+                    }
+                }
             },
-        ){
-            HomeScreen()
+        ){ innerPadding ->
+            SampleAppNavHost(
+                navController = navController,
+                modifier = Modifier.padding(innerPadding)
+            )
         }
     }
 }
 
 @Composable
-fun AppBar(openDrawer: () -> Unit) {
+fun SampleAppNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = SampleAppScreen.Home.route,
+        modifier = modifier
+    ) {
+        composable(SampleAppScreen.Home.route) {
+            HomeScreen()
+        }
+        composable(SampleAppScreen.Shopping.route) {
+            ShoppingScreen()
+        }
+        composable(SampleAppScreen.Profile.route) {
+            ProfileScreen()
+        }
+    }
+}
+
+@Composable
+fun AppBar(
+    openDrawer: () -> Unit
+) {
     TopAppBar(
-        title = { Text("Home") },
+        title = { Text(stringResource(R.string.app_name)) },
         backgroundColor = MaterialTheme.colors.primary,
         navigationIcon = {
             IconButton(onClick = { openDrawer() }) {
@@ -85,15 +142,6 @@ fun AppBar(openDrawer: () -> Unit) {
         },
     )
 }
-
-private val screens = listOf(
-    "Home",
-    "Shopping",
-    "Profile",
-    "WebView",
-    "Settings",
-    "Developer"
-)
 
 @Composable
 fun MainDrawer(
@@ -107,8 +155,11 @@ fun MainDrawer(
         screens.forEach { screen ->
             Spacer(Modifier.height(24.dp))
             Text(
-                text = screen,
-                style = MaterialTheme.typography.body1
+                text = stringResource(screen.resourceId),
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.clickable {
+                    onDestinationClicked(screen.route)
+                }
             )
         }
     }
