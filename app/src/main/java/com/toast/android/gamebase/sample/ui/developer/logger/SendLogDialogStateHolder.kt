3712b6f9131ca9c9cf467dc3ae20438e060be600
JunSeok <luckystay12@nhn.com>
2022-11-10 14:34:12 +0900

    Apply code review(206d1248fe7b3d79e71e1ffc813e05e585775831)

--- app/src/main/java/com/toast/android/gamebase/sample/ui/developer/logger/SendLogDialogStateHolder.kt
+++ app/src/main/java/com/toast/android/gamebase/sample/ui/developer/logger/SendLogDialogStateHolder.kt
@@ -1,18 +1,6 @@
 package com.toast.android.gamebase.sample.ui.developer.logger
 
-import android.app.Activity
-import android.content.Context
 import androidx.compose.runtime.mutableStateOf
-import com.toast.android.gamebase.sample.gamebasemanager.initializeNhnCloudLogger
-import com.toast.android.gamebase.sample.gamebasemanager.setNhnCloudLoggerListener
-
-class LoggerInitializeDialogStateHolder() {
-    fun initializeLogger(activity: Activity, appKey: String) {
-        val context = activity as Context
-        initializeNhnCloudLogger(context, appKey)
-        setNhnCloudLoggerListener()
-    }
-}
 
 class SendLogDialogStateHolder() {
     var loggerLevelExpanded = mutableStateOf(false)
