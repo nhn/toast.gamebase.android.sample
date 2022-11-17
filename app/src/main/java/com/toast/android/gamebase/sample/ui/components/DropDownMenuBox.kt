825348572c0d3393f6a446bb7493e34679257400
박은우/게임플랫폼클라팀/NE <eunwoo.park@nhn.com>
2022-11-17 14:02:27 +0900

    Replace static number value to use resources

--- app/src/main/java/com/toast/android/gamebase/sample/ui/components/DropDownMenuBox.kt
+++ app/src/main/java/com/toast/android/gamebase/sample/ui/components/DropDownMenuBox.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.DropdownMenuItem
@@ -24,13 +23,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.toast.android.gamebase.sample.R
-import com.toast.android.gamebase.sample.ui.theme.White
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -129,18 +127,24 @@ fun DropDownMenuBoxDialog(
                 modifier = Modifier
                     .fillMaxWidth(80f)
                     .wrapContentHeight(),
-                shape = RoundedCornerShape(12.dp),
+                shape = RoundedCornerShape(
+                    dimensionResource(id = R.dimen.drop_down_menu_box_dialog_round_corner_radius)
+                ),
                 color = MaterialTheme.colors.surface
             ) {
                 Column(
-                    modifier = Modifier.padding(20.dp),
+                    modifier = Modifier.padding(
+                        dimensionResource(id = R.dimen.drop_down_menu_box_dialog_column_padding)
+                    ),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
                     Text(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(bottom = 20.dp),
+                            .padding(bottom =
+                                dimensionResource(id = R.dimen.drop_down_menu_box_dialog_title_padding_bottom)
+                            ),
                         text = title,
                         textAlign = TextAlign.Left
                     )
