7ae224d197bf2c26fdc0a25471e40e9d717ec244
박은우/게임플랫폼클라팀/NE <eunwoo.park@nhn.com>
2022-11-25 13:57:45 +0900

    Move resource declaration in AccessInformationScreen

--- app/src/main/java/com/toast/android/gamebase/sample/ui/access_info/AccessInformationScreen.kt
+++ app/src/main/java/com/toast/android/gamebase/sample/ui/access_info/AccessInformationScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.toast.android.gamebase.sample.R
@@ -43,22 +44,24 @@ fun AccessInformationScreen(updateVersionInPreferenceAndState: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Column(
-                modifier = Modifier.padding(top = 50.dp),
+                modifier = Modifier.padding(
+                    top = dimensionResource(id = R.dimen.access_info_screen_padding_top)),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
                     text = stringResource(R.string.access_info_title),
                     color = Black,
-                    fontSize = 30.sp,
+                    fontSize = dimensionResource(id = R.dimen.access_info_title_font_size).value.sp,
                     fontWeight = FontWeight(400),
                     textAlign = TextAlign.Center
                 )
                 Text(
-                    modifier = Modifier.padding(top = 20.dp),
+                    modifier = Modifier.padding(
+                        top = dimensionResource(id = R.dimen.access_info_sub_title_padding_top)),
                     text = stringResource(R.string.access_info_subtitle),
                     color = Grey500,
-                    fontSize = 15.sp,
+                    fontSize = dimensionResource(id = R.dimen.access_info_sub_title_font_size).value.sp,
                     fontWeight = FontWeight(300),
                     textAlign = TextAlign.Center
                 )
@@ -66,12 +69,12 @@ fun AccessInformationScreen(updateVersionInPreferenceAndState: () -> Unit) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(24.dp)
+                    .padding(dimensionResource(id = R.dimen.access_info_content_padding))
             ) {
                 Text(
                     text = stringResource(R.string.access_info_list_title),
                     color = Black,
-                    fontSize = 25.sp,
+                    fontSize = dimensionResource(id = R.dimen.access_info_content_font_size).value.sp,
                     fontWeight = FontWeight(350),
                     textAlign = TextAlign.Start
                 )
@@ -79,10 +82,10 @@ fun AccessInformationScreen(updateVersionInPreferenceAndState: () -> Unit) {
                     ListAccessInformation(information = AccessInformation)
                 }
             }
-            Spacer(modifier = Modifier.height(70.dp))
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.access_info_button_spacer)))
             Button(
                 modifier = Modifier
-                    .height(70.dp)
+                    .height(dimensionResource(id = R.dimen.access_info_button_height))
                     .fillMaxSize(),
                 onClick = updateVersionInPreferenceAndState,
                 colors = ButtonDefaults.buttonColors(backgroundColor = Toast),
@@ -90,7 +93,7 @@ fun AccessInformationScreen(updateVersionInPreferenceAndState: () -> Unit) {
                 Text(
                     text = stringResource(R.string.button_ok),
                     color = White,
-                    fontSize = 30.sp,
+                    fontSize = dimensionResource(id = R.dimen.access_info_button_font_size).value.sp,
                     fontWeight = FontWeight(400),
                     textAlign = TextAlign.Center
                 )
@@ -104,31 +107,31 @@ fun ListAccessInformation(information: AccessInformation) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 20.dp),
+            .padding(top = dimensionResource(id = R.dimen.access_info_list_padding)),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.access_info_list_item_space))
     ) {
         Image(
             painter = painterResource(information.imgSrc),
             contentDescription = stringResource(information.contentDescription),
             contentScale = ContentScale.Fit,
             modifier = Modifier
-                .width(60.dp)
-                .height(60.dp)
+                .width(dimensionResource(id = R.dimen.access_info_image_icon_size))
+                .height(dimensionResource(id = R.dimen.access_info_image_icon_size))
                 .background(color = Grey300, shape = CircleShape)
-                .padding(10.dp)
+                .padding(dimensionResource(id = R.dimen.access_info_image_icon_padding))
         )
         Column {
             Text(
                 text = stringResource(information.title),
                 color = Black,
-                fontSize = 15.sp,
+                fontSize = dimensionResource(id = R.dimen.access_info_list_item_title_font_size).value.sp,
                 fontWeight = FontWeight(300)
             )
             Text(
                 text = stringResource(information.subTitle),
                 color = Grey500,
-                fontSize = 13.sp,
+                fontSize = dimensionResource(id = R.dimen.access_info_list_item_sub_title_font_size).value.sp,
                 fontWeight = FontWeight(300)
             )
         }
