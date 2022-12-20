bfd9f8af91f0d6b79a340d6f021421880b3dfc2e
박은우/게임플랫폼클라팀/NE <eunwoo.park@nhn.com>
2022-12-20 20:37:03 +0900

    Add deploy file for release public github

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -p "version : " version
+
+cd ../toast.gamebase.android.sample
+
+git remote add sample ../gamebase.client.android.sample
+git remote update
+git merge sample/master -Xtheirs --allow-unrelated-histories --squash --no-edit
+git commit -m "Update Gamebase Android SDK $version"
+git rm deploy.sh
+git add .
+git commit --amend --no-edit --allow-empty
+git push -u origin master
+git remote remove sample
+
+curl --location --request POST 'https://api.github.com/repos/nhn/toast.gamebase.android.sample/releases' \
+--header 'Accept: application/vnd.github.v3+json' \
+--header 'Authorization: token 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' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "target_commitish": "master",
+    "tag_name" : "'$version'",
+    "name" : "'$version'",
+    "body" : "Release Note : https://docs.toast.com/ko/Game/Gamebase/ko/release-notes-android/"
+}'
