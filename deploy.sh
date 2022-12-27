2ff3eb5338ec6f514f2f78183a15f1dc3fb6d77d
박은우/게임플랫폼클라팀/NE <eunwoo.park@nhn.com>
2022-12-27 15:40:35 +0900

    Fix branch for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ cd ../toast.gamebase.android.sample
 
 git remote add sample ../gamebase.client.android.sample
 git remote update
-git merge sample/master -Xtheirs --allow-unrelated-histories --squash --no-edit
+git merge sample/develop -Xtheirs --allow-unrelated-histories --squash --no-edit
 git commit -m "Update Gamebase Android SDK $version"
 git rm deploy.sh
 git add .
 git commit --amend --no-edit --allow-empty
-git push -u origin master
+git push -u origin main
 git remote remove sample
 
 curl --location --request POST 'https://api.github.com/repos/nhn/toast.gamebase.android.sample/releases' \
@@ -19,7 +19,7 @@ curl --location --request POST 'https://api.github.com/repos/nhn/toast.gamebase.
 --header 'Authorization: token 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' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "target_commitish": "master",
+    "target_commitish": "main",
     "tag_name" : "'$version'",
     "name" : "'$version'",
     "body" : "Release Note : https://docs.toast.com/ko/Game/Gamebase/ko/release-notes-android/"
