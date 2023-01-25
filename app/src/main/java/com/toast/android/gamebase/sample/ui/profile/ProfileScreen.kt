e72bcb98e805ff712320e34d94d6a7f11764e3d3
박은우/게임플랫폼클라팀/NE <eunwoo.park@nhn.com>
2023-01-25 17:57:28 +0900

    Add clipboard functionality for profile screen's user Id & access token

--- app/src/main/java/com/toast/android/gamebase/sample/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/toast/android/gamebase/sample/ui/profile/ProfileScreen.kt
@@ -1,6 +1,7 @@
 package com.toast.android.gamebase.sample.ui.profile
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,7 +27,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.toast.android.gamebase.sample.R
+import com.toast.android.gamebase.sample.ui.components.text.ClickableText
 import com.toast.android.gamebase.sample.ui.theme.Grey700
+import copyClipBoard
 
 @Composable
 fun ProfileScreen(
@@ -50,22 +55,30 @@ fun ProfileScreen(
                 .align(Alignment.CenterHorizontally))
 
         Spacer(Modifier.height(dimensionResource(id = R.dimen.profile_image_space_bottom)))
-
+        val context = LocalContext.current
         Text(text = stringResource(R.string.profile_menu_gamebase_user_id), fontWeight = FontWeight.Bold)
-        Text(text = profileViewModel.userId, color = Grey700)
+        ClickableText(
+            text = profileViewModel.userId,
+            showPadding = false,
+            style = MaterialTheme.typography.body1,
+            onClick = { copyClipBoard(context, profileViewModel.userId) })
         Spacer(Modifier.height(dimensionResource(id = R.dimen.profile_item_spacer)))
 
         Text(text = stringResource(R.string.profile_menu_gamebase_access_token), fontWeight = FontWeight.Bold)
-        Text(text = profileViewModel.accessToken, color = Grey700)
+        ClickableText(
+            text = profileViewModel.accessToken,
+            showPadding = false,
+            style = MaterialTheme.typography.body1,
+            onClick = { copyClipBoard(context, profileViewModel.accessToken) })
         Spacer(Modifier.height(dimensionResource(id = R.dimen.profile_item_spacer)))
 
         Text(text = stringResource(R.string.profile_menu_last_logged_in_provider), fontWeight = FontWeight.Bold)
-        Text(text = profileViewModel.lastLoggedInProvider, color = Grey700)
+        Text(text = profileViewModel.lastLoggedInProvider, style = MaterialTheme.typography.body1)
         Spacer(Modifier.height(dimensionResource(id = R.dimen.profile_item_spacer)))
 
         Text(text = stringResource(R.string.profile_menu_connected_idp_list), fontWeight = FontWeight.Bold)
         for (idp in profileViewModel.connectedIdpList) {
-            Text(text = idp, color = Grey700)
+            Text(text = idp, style = MaterialTheme.typography.body1)
         }
     }
 }
---
+++ app/src/main/java/com/toast/android/gamebase/sample/util/ClipboardUtil.kt
@@ -0,0 +1,19 @@
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
+
+fun copyClipBoard(context: Context, string: String) {
+    val appContext = context.applicationContext
+    val clipboardManager = appContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clipData = ClipData.newPlainText("text", string)
+    clipboardManager.setPrimaryClip(clipData)
+    val data = clipboardManager.primaryClip
+    data?.let {
+        showToastMessage(context, "Copy Clipboard : " + it.getItemAt(0).text.toString())
+    }
+}
+
+fun showToastMessage(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
